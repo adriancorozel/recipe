abfda0484a218a738bf469d8800ba2db5f920096
alex-pasaila <alexpasa2003@yahoo.com>
2020-07-02 16:16:50 +0300

    changed name of the app and port... and color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'user-manager-app';
+  title = 'digital-recipe-app';
   isLoggedIn: boolean;
 
   constructor(_authService: AuthService) {
